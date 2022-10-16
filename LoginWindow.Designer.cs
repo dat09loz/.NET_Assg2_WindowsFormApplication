5ad38686598b24b5ded285ab6d50edcd60da5eb7
dat09loz <61342271+dat09loz@users.noreply.github.com>
2022-10-16 23:16:31 +1100

    login window completed

---
+++ LoginWindow.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Assg2_WindowsFormApplication
+{
+    partial class LoginWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.NewUserBtn = new System.Windows.Forms.Button();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(96, 73);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(60, 15);
+            this.Username.TabIndex = 0;
+            this.Username.Text = "Username";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(96, 104);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(57, 15);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "Password";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.UsernameTextBox.Location = new System.Drawing.Point(171, 70);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.PlaceholderText = "Enter username";
+            this.UsernameTextBox.Size = new System.Drawing.Size(136, 23);
+            this.UsernameTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PasswordTextBox.Location = new System.Drawing.Point(171, 101);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PlaceholderText = "Enter password";
+            this.PasswordTextBox.Size = new System.Drawing.Size(136, 23);
+            this.PasswordTextBox.TabIndex = 3;
+            this.PasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // NewUserBtn
+            // 
+            this.NewUserBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NewUserBtn.Location = new System.Drawing.Point(12, 12);
+            this.NewUserBtn.Name = "NewUserBtn";
+            this.NewUserBtn.Size = new System.Drawing.Size(75, 23);
+            this.NewUserBtn.TabIndex = 4;
+            this.NewUserBtn.Text = "New User";
+            this.NewUserBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoginBtn.Location = new System.Drawing.Point(115, 147);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoginBtn.TabIndex = 5;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitBtn.Location = new System.Drawing.Point(214, 147);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 6;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // LoginWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 231);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.NewUserBtn);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "LoginWindow";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label Username;
+        private Label Password;
+        private TextBox UsernameTextBox;
+        private TextBox PasswordTextBox;
+        private Button NewUserBtn;
+        private Button LoginBtn;
+        private Button ExitBtn;
+    }
+}
---
+++ LoginWindow.cs
@@ -0,0 +1,47 @@
+namespace Assg2_WindowsFormApplication
+{
+    public partial class LoginWindow : Form
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            //check credentials
+            bool success = false;
+            string[] splitChar = {" ", ","};
+            string[] users = File.ReadAllLines("login.txt");
+            foreach (string user in users)
+            {
+                string[] userInfo = user.Split(splitChar, StringSplitOptions.RemoveEmptyEntries); //convert each line to an array, with splitChar as split indicator
+                if (UsernameTextBox.Text == userInfo[0] && PasswordTextBox.Text == userInfo[1]) //correct username and password?
+                {
+                    success = true;
+                    // Hide();
+                    MessageBox.Show("Login successful!", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            //login failed
+            if (!success)
+            {
+                MessageBox.Show("Invalid Credentials!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //clear textbox
+            UsernameTextBox.Text = string.Empty;
+            PasswordTextBox.Text = string.Empty;
+        }
+
+        private void NewUserBtn_Click(object sender, EventArgs e)
+        {
+            Hide();
+            //create account windows appears...
+        }
+    }
+}
